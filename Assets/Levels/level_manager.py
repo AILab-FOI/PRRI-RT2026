cf25416ab18c12954b6546e6471f012ccab30be8
Bartol Kolakovic <gamerhrvatska.dj@gmail.com>
2026-04-08 12:49:38 +0200

    Level struct change
    Testiranje za veliki RAM koristenje. Nije ovo bio problem.

--- Assets/Levels/level_manager.py
+++ Assets/Levels/level_manager.py
@@ -1,6 +1,5 @@
 import os
 import importlib
-import pkgutil
 import re
 
 from Assets.scripts.Character.interaction import InteractiveObject
@@ -10,7 +9,7 @@ from Assets.npcs.dialogue_npc import create_dialogue_npcs
 class LevelManager:
     def __init__(self, game):
         self.game = game
-        self.current_level = 2
+        self.current_level = 1
         self.level_data = {}
         self.max_level = 0 #deprecated trebalo bi biti automatic
         self.current_weapon_type = 'pistol'
@@ -35,10 +34,10 @@ class LevelManager:
             for folder in level_folders:
                 match = re.match(r'^Lvl(\d+)$',folder)
                 level_num = int(match.group(1))
-                module_path = f'Assets.Levels.Lvl{level_num}.level{level_num}'
+                module_path = f'Assets.Levels.level{level_num}'
                 try:
                     print(f"Importing level {level_num} from {module_path}")
-                    level_module = importlib.import_module(module_path)
+                    level_module =importlib.import_module(module_path)
                     self.level_data[level_num] = level_module.get_level_data()
                 except ImportError as e:
                     from Assets.Levels.base_level import create_base_level_structure
@@ -47,7 +46,7 @@ class LevelManager:
 
             self.max_level = max(self.level_data.keys(), default=0)
             print(f"Loaded {len(self.level_data)} levels. Max level: {self.max_level}")
-
+            
 
             """
             Old #Ostvaviti za svaku slucaj 
@@ -61,10 +60,11 @@ class LevelManager:
                     print("import error  level ",level_num)
                     self.level_data[level_num] = create_base_level_structure()
             """
-        except Exception:
+        except Exception as e:
             print(f"Critical error during level initialization: {e}")
             self.level_data = {}
             self.max_level = 0
+        
 
     def load_level(self, level_number):
         if level_number in self.level_data:
